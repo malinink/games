a87a20c8d67df63fb8c148e148c14c876d444ed7
malinink <apmath@malinink.ru>
2016-02-17 20:58:21 +0300

    роуты для авторизации и аутентификации

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -26,6 +26,10 @@ Route::get('/', function () {
 |
 */
 
-Route::group(['middleware' => ['web']], function () {
-    //
+Route::group(['middleware' => 'web'], function () {
+    
+    Route::auth();
+    
+    Route::get('/home', 'HomeController@index');
+    
 });
