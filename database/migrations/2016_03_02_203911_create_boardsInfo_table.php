eed2e3647a52384d3585bce1d800687ecce0f99f
AnanasKelly <anastasiya_johnni@mail.ru>
2016-03-02 23:44:23 +0300

    add migrations

---
+++ database/migrations/2016_03_02_203911_create_boardsInfo_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBoardsInfoTable extends Migration
+{
+    public function up()
+    {     
+        Schema::create(
+            'boardsInfo',
+            function (Blueprint $table) {
+                $table->integer('game_id')->unsigned;
+                $table->foreign('game_id')->references('id')->on('games')->onDelete('cascade');
+                $table->tinyInteger('figure');
+                $table->smallInteger('position');
+                $table->boolean('color');
+                $table->boolean('special');
+                $table->smallInteger('turn_number');
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('boardsInfo');
+    }
+}
