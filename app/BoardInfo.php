80d1490bc7c88733e05de0c4f9eb39efae8f23d5
AnanasKelly <anastasiya_johnni@mail.ru>
2016-03-02 23:43:01 +0300

    add model BoardInfo

---
+++ app/BoardInfo.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BoardInfo extends Model
+{
+    protected $table = 'boardsInfo';
+    protected $fillable = array(
+        'game_id',
+        'figure',
+        'position',
+        'color',
+        'special',
+        'turn_number'
+    );
+    
+    public function games()
+    {
+        return $this->belongsTo('App\Games');
+    }
+  
+}
