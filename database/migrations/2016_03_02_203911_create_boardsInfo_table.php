3d16271fe6fb0195c648e33c5e3b72a5bcf2a73e
AnanasKelly <anastasiya_johnni@mail.ru>
2016-03-03 00:03:57 +0300

    fix phpcs

--- database/migrations/2016_03_02_203911_create_boardsInfo_table.php
+++ database/migrations/2016_03_02_203911_create_boardsInfo_table.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 class CreateBoardsInfoTable extends Migration
 {
     public function up()
-    {     
+    {
         Schema::create(
             'boardsInfo',
             function (Blueprint $table) {
