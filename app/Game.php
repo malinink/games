4dbd24f1538b44d941e468535b8f37fd8ae7aecb
AnanasKelly <anastasiya_johnni@mail.ru>
2016-03-04 23:22:10 +0300

    add game model

---
+++ app/Game.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Game extends Model
+{
+    protected $table = 'games';
+    protected $fillable = array(
+        'id',
+        'type_id',
+        'private',
+        'time_started',
+        'time_finished',
+        'winner',
+        'bonus'
+    );
+    
+    public function boardsInfo()
+    {
+        return $this->hasMany('App\BoardInfo');
+    }
+    
+    public function turnInfo()
+    {
+        return $this->hasMany('App\TurnInfo');
+    }
+    
+    public function usergames()
+    {
+        return $this->hasMany('App\UserGame');
+    }
+    
+    public function gamesType()
+    {
+        return $this->belongsTo('App\GameType');
+    }
+}
