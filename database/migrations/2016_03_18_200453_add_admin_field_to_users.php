4cc25583366eec4816d61eb075d6ebb9df27794a
artesby <artesby1@gmail.com>
2016-03-24 02:28:36 +0300

    fix field type

--- database/migrations/2016_03_18_200453_add_admin_field_to_users.php
+++ database/migrations/2016_03_18_200453_add_admin_field_to_users.php
@@ -16,7 +16,7 @@ class AddAdminFieldToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('isAdmin')->default('0');
+            $table->boolean('isAdmin')->default(false);
         });
     }
 
