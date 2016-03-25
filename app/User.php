736708b7000170402be54f0eff861ccc88177372
artesby <artesby1@gmail.com>
2016-03-25 20:48:45 +0300

    add game status function

--- app/User.php
+++ app/User.php
@@ -40,26 +40,58 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
     
-    public function userdefault()
+    /**
+     * Show if user $id has an active game:
+     * 0 - no game; 1 - searching for opponent; 2 - playing match
+     *
+     * @param int $id
+     * @return int
+     */
+    public static function userHasGame(User $user)
     {
-        return $this->hasOne('App\UserDefault');
+        $lastGame = $user->usergames->last()->game;
+        if (is_null($lastGame->time_finished)) {
+            if (count($lastGame->usergames->all()) > 1) {
+                return 2;
+            } else {
+                return 1;
+            }
+        } else {
+            return 0;
+        }
     }
     
+    /**
+     *
+     * @return UserIngameInfo[]
+     */
     public function useringameinfos()
     {
         return $this->hasMany('App\UserIngameInfo');
     }
     
+    /**
+     *
+     * @return UserGame[]
+     */
     public function usergames()
     {
         return $this->hasMany('App\UserGame');
     }
     
+    /**
+     *
+     * @return GameType[]
+     */
     public function gametypes()
     {
         return $this->belongsTo('App\GameType');
     }
     
+    /**
+     *
+     * @return User[]
+     */
     public function friends()
     {
         return $this->belongsToMany('App\User', 'friends_users', 'friend_id', 'user_id');
