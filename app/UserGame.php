ae2278d0342860f2a8551ca48ff951ff021e4197
malinink <apmath@malinink.ru>
2016-03-28 11:47:46 +0300

    fix UserGame model

--- app/UserGame.php
+++ app/UserGame.php
@@ -15,16 +15,39 @@ class UserGame extends Model
      * @var string
      */
     protected $table ='user_games';
-    
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
-        'user_id', 'game_id', 'color', 'reserve_time',
+        'user_id',
+        'game_id',
+        'color',
+        'reserve_time',
     ];
     
+    /**
+     * Disable Timestamps fields
+     *
+     * @var boolean
+     */
+    public $timestamps = false;
+
+    /**
+     *
+     * @return User
+     */
     public function user()
     {
         return $this->belongsTo('App\User');
     }
     
+    /**
+     *
+     * @return Game
+     */
     public function game()
     {
         return $this->belongsTo('App\Game');
