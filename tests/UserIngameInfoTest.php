cf3f5ad261028e6a9e3846c6943d0fdb2f7b0c6c
Iren <nor.iren@yandex.ru>
2016-03-30 23:38:03 +0300

    Fix UserIngameInfoTest

--- tests/UserIngameInfoTest.php
+++ tests/UserIngameInfoTest.php
@@ -14,19 +14,26 @@ class UserIngameInfoTest extends TestCase
     protected $gameTypesId2;
     protected $inGameInfo2Id;
     
-    protected $Gametype1;
-    protected $Gametype2;
-    
-    protected $user1;
-    protected $user2;
-    
-    protected $inGameInfo1;
-    protected $inGameInfo2;
-    
-    protected $inGameInfo3;
-    protected $inGameInfo3Id;
     protected $userId3;
+    protected $inGameInfo3Id;
     protected $gameTypesId3;
+    
+    protected $trashId1;
+    protected $trashId2;
+    protected $trashId3;
+    protected $trashId4;
+    
+    /**
+     * Prepare for testing
+     *
+     *  @return void
+     */
+    public function testPrepare()
+    {
+        DB::statement('ALTER TABLE game_types AUTO_INCREMENT=1;');
+        DB::statement('ALTER TABLE user_ingame_infos AUTO_INCREMENT=1;');
+        DB::statement('ALTER TABLE users AUTO_INCREMENT=1;');
+    }
     /**
      * Create new data
      *
@@ -34,59 +41,44 @@ class UserIngameInfoTest extends TestCase
      */
     public function testCreate()
     {
-        DB::statement('ALTER TABLE game_types AUTO_INCREMENT=1;');
-        DB::statement('ALTER TABLE user_ingame_infos AUTO_INCREMENT=1;');
-        DB::statement('ALTER TABLE users AUTO_INCREMENT=1;');
-        
         global $userId1, $gameTypesId1,$userId2,$gameTypesId2,
             $inGameInfo1Id,$inGameInfo2Id,$inGameInfo3Id,
-            $gameTypesId3,$userId3;
+            $gameTypesId3,$userId3,$trashId1,$trashId2,$trashId3,$trashId4;
         
         //With links
         
-        $Gametype1 = factory(App\GameType::class)->make();
-        $Gametype1->save();
-        
-        $Gametype2 = factory(App\GameType::class)->make();
-        $Gametype2->save();
-        
-        $user1 = factory(App\User::class)->make();
-        $user1->save();
-        
-        $user2 = factory(App\User::class)->make();
-        $user2->save();
+        $gametype1 = factory(App\GameType::class)->create();
+        $gametype2 = factory(App\GameType::class)->create();
+        $user1 = factory(App\User::class)->create();
+        $user2 = factory(App\User::class)->create();
         
         $userId1=$user1->id;
         $userId2=$user2->id;
+        $gameTypesId1=$gametype1->id;
+        $gameTypesId2=$gametype2->id;
         
-        $gameTypesId1=$Gametype1->id;
-        $gameTypesId2=$Gametype2->id;
-        
-        $inGameInfo1= factory(App\UserIngameInfo::class)->make()
-            ->user()->associate($userId1)->gameType()->associate($gameTypesId1);
-        
+        $inGameInfo1= factory(App\UserIngameInfo::class)->make();
+        $trashId1=$inGameInfo1->user_id;
+        $trashId2=$inGameInfo1->game_type_id;
+        $inGameInfo1->user()->associate($userId1)
+            ->gameType()->associate($gameTypesId1);
         $inGameInfo1->save();
         
-        $inGameInfo2= factory(App\UserIngameInfo::class)->make()
-            ->user()->associate($userId2)->gameType()->associate($gameTypesId2);
-        
+        $inGameInfo2= factory(App\UserIngameInfo::class)->make();
+        $trashId3=$inGameInfo2->user_id;
+        $trashId4=$inGameInfo2->game_type_id;
+        $inGameInfo2->user()->associate($userId2)
+            ->gameType()->associate($gameTypesId2);
         $inGameInfo2->save();
        
         $inGameInfo1Id=$inGameInfo1->id;
         $inGameInfo2Id=$inGameInfo2->id;
         
         //Without links
-        
-        $gameTypesId3 = factory(App\GameType::class)->create()->id;
-        
-        $userId3 = factory(App\User::class)->create()->id;
-     
-        $inGameInfo3= factory(App\UserIngameInfo::class)->make()
-            ->user()->associate($userId3)->gameType()->associate($gameTypesId3);
-        
-        $inGameInfo3->save();
+        $inGameInfo3= factory(App\UserIngameInfo::class)->create();
+        $userId3=$inGameInfo3->user_id;
+        $gameTypesId3=$inGameInfo3->game_type_id;
         $inGameInfo3Id=$inGameInfo3->id;
-        
     }
     
     /**
@@ -104,21 +96,25 @@ class UserIngameInfoTest extends TestCase
         );
         
         //testing from one side
-        $info=  App\UserIngameInfo::find($inGameInfo1Id);
-        $Emil=$info->user;
-        $this->assertEquals($info->user_id, $Emil->id);
-        
-        $chess=$info->gameType;
-        $this->assertEquals($info->game_type_id, $chess->id);
+        $this->assertEquals(
+            App\UserIngameInfo::find($inGameInfo1Id)->user_id,
+            App\UserIngameInfo::find($inGameInfo1Id)->user->id
+        );
+        $this->assertEquals(
+            App\UserIngameInfo::find($inGameInfo1Id)->game_type_id,
+            App\UserIngameInfo::find($inGameInfo1Id)->gameType->id
+        );
         
         //testing from other side
-        $Berwald=  App\User::find($userId1);
-        $infoBerwald=$Berwald->userIngameInfos()->first();
-        $this->assertEquals($Berwald->id, $infoBerwald->user_id);
-        
-        $gameTino= App\GameType::find($gameTypesId1);
-        $infoTino=$gameTino->userIngameInfos()->first();
-        $this->assertEquals($gameTino->id, $infoTino->game_type_id);
+        $this->assertEquals(
+            App\User::find($userId1)->id,
+            App\User::find($userId1)->userIngameInfos()->first()->user_id
+        );
+        $this->assertEquals(
+            App\GameType::find($gameTypesId1)->id,
+            App\GameType::find($gameTypesId1)->userIngameInfos()
+                ->first()->game_type_id
+        );
     }
     /**
      * Check connection
@@ -135,22 +131,25 @@ class UserIngameInfoTest extends TestCase
         );
         
         //testing from one side
-        $info=  App\UserIngameInfo::find($inGameInfo2Id);
-        $Lucas=$info->user;
-        $this->assertEquals($info->user_id, $Lucas->id);
-        
-        $chess=$info->gameType;
-        $this->assertEquals($info->game_type_id, $chess->id);
-        
+        $this->assertEquals(
+            App\UserIngameInfo::find($inGameInfo2Id)->user_id,
+            App\UserIngameInfo::find($inGameInfo2Id)->user->id
+        );
+        $this->assertEquals(
+            App\UserIngameInfo::find($inGameInfo2Id)->game_type_id,
+            App\UserIngameInfo::find($inGameInfo2Id)->gameType->id
+        );
         
         //testing from other side
-        $Berwald=  App\User::find($userId2);
-        $infoBerwald=$Berwald->userIngameInfos()->first();
-        $this->assertEquals($Berwald->id, $infoBerwald->user_id);
-        
-        $gameTino= App\GameType::find($gameTypesId2);
-        $infoTino=$gameTino->userIngameInfos()->first();
-        $this->assertEquals($gameTino->id, $infoTino->game_type_id);
+        $this->assertEquals(
+            App\User::find($userId2)->id,
+            App\User::find($userId2)->userIngameInfos()->first()->user_id
+        );
+        $this->assertEquals(
+            App\GameType::find($gameTypesId2)->id,
+            App\GameType::find($gameTypesId2)->userIngameInfos()
+                ->first()->game_type_id
+        );
     }
     /**
      * Check connection
@@ -162,22 +161,25 @@ class UserIngameInfoTest extends TestCase
         global $gameTypesId3,$userId3,$inGameInfo3Id;
         
         //testing from one side
-        $info= App\UserIngameInfo::find($inGameInfo3Id);
-        $Lucas=$info->user;
-        $this->assertEquals($info->user_id, $Lucas->id);
-        
-        $chess=$info->gameType;
-        $this->assertEquals($info->game_type_id, $chess->id);
+        $this->assertEquals(
+            App\UserIngameInfo::find($inGameInfo3Id)->user_id,
+            App\UserIngameInfo::find($inGameInfo3Id)->user->id
+        );
+        $this->assertEquals(
+            App\UserIngameInfo::find($inGameInfo3Id)->game_type_id,
+            App\UserIngameInfo::find($inGameInfo3Id)->gameType->id
+        );
         
         //testing from other side
-        $Berwald=  App\User::find($userId3);
-        $infoBerwald=$Berwald->userIngameInfos()->first();
-        $this->assertEquals($Berwald->id, $infoBerwald->user_id);
-        
-        $gameTino= App\GameType::find($gameTypesId3);
-        $infoTino=$gameTino->userIngameInfos()->first();
-        $this->assertEquals($gameTino->id, $infoTino->game_type_id);
-    
+        $this->assertEquals(
+            App\User::find($userId3)->id,
+            App\User::find($userId3)->userIngameInfos()->first()->user_id
+        );
+        $this->assertEquals(
+            App\GameType::find($gameTypesId3)->id,
+            App\GameType::find($gameTypesId3)->userIngameInfos()
+                ->first()->game_type_id
+        );
     }
     /**
      * Delete data
@@ -188,7 +190,8 @@ class UserIngameInfoTest extends TestCase
     public function testRemove()
     {
         global $userId1,$userId2,$inGameInfo1Id,$inGameInfo2Id,
-            $inGameInfo3Id, $gameTypesId1,$gameTypesId2;
+            $inGameInfo3Id, $gameTypesId1,$gameTypesId2,
+            $trashId1,$trashId2,$trashId3,$trashId4;
         
         App\UserIngameInfo::find($inGameInfo1Id)->delete();
         App\UserIngameInfo::find($inGameInfo2Id)->delete();
@@ -196,6 +199,11 @@ class UserIngameInfoTest extends TestCase
         App\GameType::find($gameTypesId2)->delete();
         App\User::find($userId1)->delete();
         App\User::find($userId2)->delete();
+        
+        App\GameType::find($trashId2)->delete();
+        App\GameType::find($trashId4)->delete();
+        App\User::find($trashId1)->delete();
+        App\User::find($trashId3)->delete();
         
         $info=App\UserIngameInfo::find($inGameInfo3Id);
         $info->delete();
