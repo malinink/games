1e9c133b69a13b919aa245689614453c3042d45b
Iren <nor.iren@yandex.ru>
2016-04-02 23:21:03 +0300

    Add UserIngameInfoWithUserRelationTest

---
+++ tests/UserIngameInfoWithUserRelationTest.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ *
+ * @author IrenJones
+ */
+
+use Illuminate\Support\Collection;
+use App\UserIngameInfo;
+
+class UserIngameInfoWithUserRelationTest extends TestCase
+{
+    /**
+     *
+     * @var UserIngameInfo
+     */
+    protected $userInGameInfo;
+   
+    public function setUp()
+    {
+        parent::setUp();
+        $this->userInGameInfo=factory(UserIngameInfo::class)->create();
+    }
+    
+    public function testUserIngameInfoToUserRelation()
+    {
+        $userInGameInfo=$this->userInGameInfo;
+        
+        $this->assertEquals($userInGameInfo->user_id, $userInGameInfo->user->id);
+    }
+    
+    public function testUserToUserIngameInfosIsCollection()
+    {
+        $userInGameInfo=$this->userInGameInfo;
+        $user=$userInGameInfo->user;
+        
+        $this->assertTrue($user->userIngameInfos instanceof Collection);
+    }
+    
+    /**
+     *
+     * @depends testUserToUserIngameInfoIsCollection
+     */
+    public function testUserToUserIngameInfosRelation()
+    {
+        $userInGameInfo=$this->userInGameInfo;
+        $user=$userInGameInfo->user;
+        
+        $this->assertTrue($user->userIngameInfos->contains($userInGameInfo));
+    }
+   
+    public function tearDown()
+    {
+        $userInGameInfo=$this->userInGameInfo;
+        $userInGameInfo->delete();
+        $userInGameInfo->user->delete();
+        $userInGameInfo->gameType->delete();
+        
+        parent::tearDown();
+    }
+}
