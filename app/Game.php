881e2b12232c23593f277e1ad23bc769ee915586
AnanasKelly <anastasiya_johnni@mail.ru>
2016-04-11 21:23:21 +0300

    add create and delete methods

--- app/Game.php
+++ app/Game.php
@@ -37,7 +37,53 @@ class Game extends Model
      * @var boolean
      */
     public $timestamps = false;
-
+    
+    /**
+     *
+     * @return void
+     */
+    public static function createGame(GameType $gameType, $private)
+    {
+        /**
+         *
+         * @var User
+         */
+        $user = Auth::user();
+        $gameStatus = $user->getCurrentGameStatus();
+        switch ($gameStatus) {
+            case User::NO_GAME:
+                $game = Game::where(['private' => $private, 'game_type_id' => $gameType->id, 'time_started' => null])
+                    ->orderBy('id', 'ask')
+                    ->first();
+                if (is_null($game)) {
+                    CreateNewGame();
+                }
+                else {
+                    AddUserToGame($game);
+                }
+                break;
+            case User::SEARCH_GAME:
+                $lastUserGame = $user->userGames->sortBy('id')->last();
+                $game = Game::find($lastUserGame->game_id);
+                $game->private = $private;
+                $game->gameType()->associate($gameType);
+                $game->save();
+                break;
+        }
+                        
+    }
+    
+    /**
+     *
+     * @return void
+     */
+    public function cancelGame()
+    {
+        if (is_null($this->time_started))
+        {
+            $this->delete();
+        } 
+    }
     /**
      *
      * @return BoardInfo[]
