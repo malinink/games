25b937f0141428190b292d71b510730da3ac1df0
learp <learp@yandex.ru>
2016-04-19 23:46:17 +0300

    Add Token model

---
+++ app/Token.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ *
+ * @author learp
+ */
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Token extends Model
+{
+    /**
+     * Table Name
+     *
+     * @var string
+     */
+    protected $table = "tokens";
+    
+    const LIFETIME = 30;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        "token",
+        "user_id",
+        "expiration_date"
+    ];
+    
+    /**
+     * 
+     * @return User
+     */
+    public function user()
+    {
+        return $this->belongsTo("App\User");
+    }
+}
