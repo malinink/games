984bbdcafd65e29559d96d9c2dbe19503c8a42ab
artesby <artesby1@gmail.com>
2016-04-19 19:23:19 +0300

    turn protocol

---
+++ app/Sockets/Protocol/TurnProtocol.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ *
+ * @author artesby
+ */
+
+namespace App\Sockets\Protocol;
+
+use Ratchet\ConnectionInterface;
+use App\Sockets\PushServerSocket;
+
+class TurnProtocol extends PushserverSocket implements ProtocolInterface
+{
+    /**
+     * WS server.
+     *
+     * @var PushServerSocket
+     */
+    protected $server;
+    
+    /**
+     * Information about turn.
+     *
+     * @var array
+     */
+    protected $data;
+
+    /**
+     * Create a new TurnProtocol instance.
+     *
+     * @param array $data
+     * @param ConnectionInterface $client
+     * @param PushServerSocket $server
+     * @return void
+     */
+    public function __construct(array $data, ConnectionInterface $client, PushServerSocket $server)
+    {
+        $this->data = $data;
+        $this->server = $server;
+    }
+    
+    /**
+     *
+     * Send information about turn to all clients.
+     *
+     * @return void
+     */
+    public function compile()
+    {
+        echo sprintf('send turn to all clients' . PHP_EOL);
+        array_unshift($this->data, ['name' => 'turn']);
+        $msg = json_encode($this->data);
+        foreach ($this->server->clients as $client) {
+            $client->send($msg);
+        }
+    }
+}
