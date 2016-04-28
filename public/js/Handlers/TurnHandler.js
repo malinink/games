4bcc08a92ab1b368a35285b0e68fda85eb9b4518
AnanasKelly <anastasiya_johnni@mail.ru>
2016-04-28 21:19:05 +0300

    add turnHandler

---
+++ public/js/Handlers/TurnHandler.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author Ananskelly
+ */
+define(function() {
+    console.log('this');
+    return {
+        compile: function(data) {
+            console.log('You are in TurnHandle module!');
+            // do staff
+        }
+    };
+});
