488aa118982c75275a1e77dea68ca13a614debee
Iren <nor.iren@yandex.ru>
2016-04-30 11:03:40 +0300

    fix

--- public/js/websockets.js
+++ public/js/websockets.js
@@ -3,6 +3,9 @@
  * @author malinink
  */
 var conn = new ReconnectingWebSocket('ws://games:8080');
+conn.reconnectInterval = 5000;
+conn.maxReconnectAttempts=10;
+
 conn.onopen = function(e) {
     console.log('connection established');
 }
