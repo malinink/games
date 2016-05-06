76542e577e8d497c14ab748c1925f5a5648d0513
AnanasKelly <anastasiya_johnni@mail.ru>
2016-05-07 00:39:25 +0300

    up

--- public/js/GameControl/turnControl.js
+++ public/js/GameControl/turnControl.js
@@ -2,11 +2,19 @@
  *
  * @author Ananaskelly
  */
-define(['./gameConfig'], function(gameConfig){
+define(['./gameConfig', '/WSQueries/sync'], function(gameConfig, sync){
     var figureSize = gameConfig.getFigureConfig().size;
     var coeff = gameConfig.getFigureConfig().coeff;
     var opposite = gameConfig.getOpposite();
     var current = gameConfig.getCurrent();
+    var revert = gameConfig.getRevert();
+    var attr = '';
+    if (revert) {
+        attr = 'data-revert-id';
+    } else {
+        attr = 'data-id';
+    }
+        
     function setIntervalTimes(callback, delay, time, custom)
     {
         var count = 0;
@@ -31,26 +39,30 @@ define(['./gameConfig'], function(gameConfig){
     }
     function move(figureId, x, y) {
         var last = $('#'+figureId).parent().attr('id');
-        $('#'+figureId).appendTo('#'+x+y);
-        $('#'+last).removeClass('busy');
-        $('#'+last).removeClass(current);
-        $('#'+x+y).addClass(current);
+        $('#'+figureId).appendTo('['+attr+'='+x+y+']');
+        $('['+attr+'='+last+']').removeClass('busy');
+        $('['+attr+'='+last+']').removeClass(current);
+        $('['+attr+'='+x+y+']').addClass(current);
         changeState();
     }
     return {
         apply: function(turnParameters) {
             var game = $('.game-info').attr('data-game');
             var turn = $('.game-info').attr('data-turn');
-            if (turnParameters.game !== game || turnParameters.prev !== turn)
+            if (turnParameters.game !== game)
                 return;
+            if (turnParameters.prev !== turn) {
+                sync.sendQuery(game, turn);
+                return;
+            }
             move(turnParameters.move.figure, turnParameters.move.x, turnParameters.move.y );
             if (turnParameters.remove.length !== 0) {
                 abroad(turnParameters.remove[0].figure);
                 var newPosition = turnParameters.move.x + turnParameters.move.y;
                 var oldPosition =  $('#'+turnParameters.remove[0].figure).parent().attr('id');
                 if (newPosition !== oldPosition) {
-                    $('#' + newPosition).addClass('busy');
-                    $('#' + oldPosition).removeClass('busy');
+                    $('['+attr+'='+newPosition+']').addClass('busy');
+                    $('['+attr+'='+oldPosition+']').removeClass('busy');
                 }
             }
             $('.game-info').attr('data-turn', turnParameters.turn);
