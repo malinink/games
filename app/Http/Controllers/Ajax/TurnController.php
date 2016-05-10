9fd72b02f18e5beaa12ade5e207321606af894b8
Iren <nor.iren@yandex.ru>
2016-05-11 00:31:29 +0300

    Fix

--- app/Http/Controllers/Ajax/TurnController.php
+++ app/Http/Controllers/Ajax/TurnController.php
@@ -5,11 +5,9 @@ namespace App\Http\Controllers;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 use App\Http\Game;
-use App\Sockets\PushServerSocket;
 
 class TurnController extends BaseController
 {
-
     /**
      * Make turn or not
      *
@@ -18,15 +16,8 @@ class TurnController extends BaseController
     public function turn(TurnRequest $msg)
     {
         $dataJs = json_decode($msg, true);
-        $type = $dataJs['name'];
         $data = $dataJs['data'];
-        $res = Game::validateTurn($data);
-        if ($res[0] === true) {
-            PushServerSocket::setDataToServer([
-                'name' => $type,
-                'data' => $res[1]
-            ]);
-        }
-        return $res[0];
+
+        return Game::validateTurn($data);
     }
 }
