1fdb9863f8d449ac2fd33cdcde84c96749877920
Iren <nor.iren@yandex.ru>
2016-05-11 14:48:47 +0300

    fix

--- public/js/changeStatus.js
+++ public/js/changeStatus.js
@@ -7,12 +7,12 @@ define(function(){
         changeStatus : function(){
         var element = document.getElementById('statusContent');
         var status=$('.stateGame').attr('value');
-        var userId = $('.board').attr('data-user');
+        var userId = parseInt($('.board').attr('data-user'));
         var playersId = [
             parseInt($('.board').attr('data-player-white')),
             parseInt($('.board').attr('data-player-black'))
         ];
-        if (playersId.indexOf(parseInt(userId))>=0) {
+        if (playersId.indexOf(userId)>=0) {
             //if gamer
             element.innerHTML = status;
         }
