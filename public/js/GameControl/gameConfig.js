5e4cde6b70dc94829bea7d4ddcd562ea4a2d9efd
AnanasKelly <anastasiya_johnni@mail.ru>
2016-05-12 23:11:20 +0300

    upd

--- public/js/GameControl/gameConfig.js
+++ public/js/GameControl/gameConfig.js
@@ -4,13 +4,37 @@
  */
 define(function(){
     var revert = false;
+    var current = 0;
+    var pawnSpecial = null;
+    var figureSize;
+    var coeff = 0.4;
     return {
-        setRevert : function(value){
+        setRevert: function(value){
             revert = value;
         },
-        getRevert : function(){
+        setCurrent: function(val){
+            current = val;
+        },
+        setPawnSpecial: function(val){
+            pawnSpecial = val;
+        },
+        setFigureSize: function(value){
+            figureSize = value;
+        },
+        getRevert: function(){
             return revert;
+        },
+        getCurrent: function(){
+            return current;
+        },
+        getPawnSpecial: function(){
+            return pawnSpecial;
+        },
+        getFigureConfig: function(){
+            return {
+                'size': figureSize,
+                'coeff': coeff
+            }
         }
     }
 })
-
