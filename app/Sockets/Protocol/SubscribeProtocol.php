a11e9e618d1133795ff7dc7ef3c0a92a1666721c
learp <learp@yandex.ru>
2016-05-13 02:04:02 +0300

    Add subscribe protocol

---
+++ app/Sockets/Protocol/SubscribeProtocol.php
@@ -0,0 +1,78 @@
+<?php
+
+/* 
+ *
+ * @author learp
+ */
+namespace App\Sockets\Protocol;
+
+use App\Sockets\PushServerSocket;
+use Ratchet\ConnectionInterface;
+use Exception;
+use App\Games;
+use App\TurnInfo;
+
+class SubscribeProtocol implements ProtocolInterface
+{
+    
+    /**
+     *
+     * @var array
+     */
+    protected $data;
+    
+    /**
+     *
+     * @var ConnectionInterface
+     */
+    protected $client;
+    
+    /**
+     *
+     * @param array $data
+     * @param ConnectionInterface $client
+     * @param PushServerSocket $server
+     * @return void
+     */
+    public function __construct(array $data, ConnectionInterface $client, PushServerSocket $server)
+    {
+        $this->data = $data;
+        $this->client = $client;
+    }
+    
+    /**
+     *
+     * Try to subscribe
+     *
+     * @return void
+     */
+    public function compile()
+    {
+        if ($this->data["type"] != "request") {
+            throw new Exception("Invalid type: not a request");
+        }
+        
+        $gameId = $this->data["game"];
+        $response = [
+                "name" => "subscribe",
+                "data" => [
+                    "type" => "response",
+                    "state" => "success"
+                ]
+            ];
+        $game = Game::find($gameId);
+            
+        if ($game === null) {
+            $response["data"]["state"] = "failed";
+        } elseif ($game->time_finished === null) {
+            $response["data"]["state"] = "unavailable";
+        }
+        
+        if ($response["data"]["state"] == "success") {
+            $turns = $game->turnInfos();
+            $response["data"]["turn"] = $turns->sortBy("turn_number")->last()->turn_number;
+        }
+        $msg = json_encode($response);
+        $this->client->send($msg);
+    }
+}
