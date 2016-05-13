b72493df996b0e0a579c146cb3d1043421ba1604
AnanasKelly <anastasiya_johnni@mail.ru>
2016-05-14 02:48:20 +0300

    fix

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@
  * @author Ananskelly
  */
 var conn;
-require(['./MessageHandler', './WSQueries/sendToken', './Ajax/getToken'], function(messageHandler, sendToken, getToken) {
+require(['./MessageHandler', './WSQueries/sendToken', './Ajax/getToken', './app'], function(messageHandler, sendToken, getToken) {
     conn = new ReconnectingWebSocket('ws://games:8080', null,
             {reconnectInterval: 5000, reconnectDecay: 1, maxReconnectAttempts: 10})
     conn.onopen = function (e) {
