0605ac1ebb3f1811c1deac98129b1f70bc896098
Iren <nor.iren@yandex.ru>
2016-05-17 23:08:43 +0300

    options_to_null

---
+++ database/migrations/2016_05_17_192558_add_nullable_to_turnInfo_options.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddNullableToTurnInfoOptions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement('ALTER TABLE turn_infos MODIFY options TINYINT NULL;');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+         Schema::table('turnInfo', function (Blueprint $table) {
+            $table->tinyInteger('options')->change();
+        });
+    }
+}
