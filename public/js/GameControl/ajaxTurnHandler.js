8a47f5ed5667375c9493868190dc6b9843979775
AnanasKelly <anastasiya_johnni@mail.ru>
2016-05-21 01:27:50 +0300

    try to handle error

---
+++ public/js/GameControl/ajaxTurnHandler.js
@@ -0,0 +1,18 @@
+/**
+ *
+ * @author Ananskelly
+ */
+define(['./gameConfig'], function(gameConfig){
+    return {
+        compile: function(data) {
+            /**console.log(JSON.parse(data));
+            if (data.data.state !== 'success'){
+                gameConfig.setConfig('activeState', true);
+                $('.cell').removeClass('cell-highlight-aim');
+                $('.cell').removeClass('cell-key');
+                // show some info 
+            }*/
+        }
+    }
+});
+
