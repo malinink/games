758c1aff08f97d62e0d5cb11a34a5b3676f922c5
AnanasKelly <anastasiya_johnni@mail.ru>
2016-05-22 03:00:10 +0300

    revert fields for killed figures

--- public/js/GameControl/setInitial.js
+++ public/js/GameControl/setInitial.js
@@ -5,6 +5,11 @@
 define(['./gameConfig'], function (gameConfig) {
     return {
         compile: function () {
+            var currentTurn = $('.game-info').attr('data-turn');
+            if (parseInt(currentTurn)%2 !== 0) {
+                gameConfig.setConfig('current', 1);
+                gameConfig.setConfig('opposite', 0);
+            }
             $('.cell').has('div.figure').addClass('busy');
             var userId = $('.board').attr('data-user');
             var white = $('.board').attr('data-player-white');
@@ -15,12 +20,16 @@ define(['./gameConfig'], function (gameConfig) {
                 if (userId === white) {
                     gameConfig.setConfig('userState', 'white');
                     gameConfig.setConfig('revert', true);
+                    $('.up').addClass('hit-white');
+                    $('.down').addClass('hit-black');
                     $('#user1').attr('data-color', 'black');
                     $('#user2').attr('data-color', 'white');
                     $('#user1-img').attr('src', '/img/black.png');
                     $('#user2-img').attr('src', '/img/white.png');
                 } else {
                     gameConfig.setConfig('userState', 'black');
+                    $('.up').addClass('hit-black');
+                    $('.down').addClass('hit-white');
                     $('#user1').attr('data-color', 'white');
                     $('#user2').attr('data-color', 'black');
                     $('#user2-img').attr('src', '/img/black.png');
